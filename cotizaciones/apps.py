da2ab696a211a6a0a8ba7f62f8bde770cb08c3c9
BenaCraftDev <josebenavidescarrillo@gmail.com>
2025-12-16 19:21:45 -0300

    Prueba Internet

--- cotizaciones/apps.py
+++ cotizaciones/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+import socket
+import requests
 
 class CotizacionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,3 +8,19 @@ class CotizacionesConfig(AppConfig):
 
     def ready(self):
         import cotizaciones.signals
+
+
+def test_internet():
+    try:
+        socket.create_connection(("www.google.com", 80), timeout=5)
+        print("✅ SOCKET: Internet OK")
+    except Exception as e:
+        print("❌ SOCKET: Sin internet:", e)
+
+    try:
+        r = requests.get("https://www.google.com", timeout=5)
+        print("✅ HTTP: Internet OK", r.status_code)
+    except Exception as e:
+        print("❌ HTTP: Sin internet:", e)
+
+test_internet()
